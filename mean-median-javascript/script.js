1e8f1078ea8ad04088c2dfea98b665d6d1c826a6
uki-dadev <marzukiberg@gmail.com>
2020-05-05 00:19:56 +0700

    Added Program Mean and Median

---
+++ mean-median-javascript/script.js
@@ -0,0 +1,89 @@
+const numberList = document.getElementById('numberList')
+const result = document.getElementById('result')
+
+const input = document.getElementById('input')
+const add = document.getElementById('add')
+const reset = document.getElementById('reset')
+
+const initialResult = []
+
+const calc = () => {
+  let total = 0
+  let arrLength = initialResult.length
+  initialResult.forEach(element => {
+    total += element
+  })
+  const mean = total / arrLength
+
+  const arrLengthNew = arrLength + 1
+
+  const compare = (a, b) => a - b
+  const urut = initialResult.sort(compare)
+  let firstIndex = 0
+  let secondIndex = 0
+  let medianIndex = 0
+  let median = 0
+  if (arrLengthNew % 2 === 1) {
+    const first = (arrLength / 2) - 1
+    const second = first + 1
+    firstIndex = urut[first]
+    secondIndex = urut[second]
+
+    median = (secondIndex + firstIndex) / 2
+  } else {
+    medianIndex = (arrLengthNew / 2) - 1
+    median = urut[medianIndex]
+  }
+
+  if (arrLength === 0) {
+    result.innerHTML = ''
+  } else {
+    result.innerHTML = `
+      <div class='alert alert-warning text-left'>
+       <span>Angka setelah diurutkan: ${urut.join(', ')}</span>
+       <br />
+       <span>Jumlah index: ${arrLength}</span>
+       <br />
+       <br />
+       <span>Mean: ${mean.toFixed(2)}</span>
+        <br />
+       <strong>Penyelesaian: ${total} / ${arrLength}</strong>
+       <br />
+       <br />
+       <span>Median: ${median}</span>
+       <br />
+       <strong>Penyelesaian: ${arrLength % 2 === 1 ? arrLengthNew +' / 2 (urutan ke-' +(medianIndex +1)+') = '+median : '('+firstIndex+'+'+secondIndex+') / 2'}</strong>
+       <br />
+       <span>dik. Jumah data adalah
+         ${arrLength % 2 === 1? 'Ganjil, kita perlu mengurutkan data terlebih dahulu, kemudian kita ambil angka pada posisi tengah, pada data angka tengah terdapat pada urutan ke <strong>'+(medianIndex+1)+'</strong>': 'Genap, kita perlu mengurutkan data terlebih dahulu, kemudian kita ambil dua angka bagian kiri dan kanan pada posisi tengah, jumlahkan dua angka tersebut kemudian bagikan dengan 2'}
+       </span>
+      </div>
+  `
+  }
+}
+
+const display = () => {
+  if (initialResult.length === 0) {
+    numberList.innerText = 'Belum ada angka untuk dihitung';
+  } else {
+    const separated = initialResult.join(', ')
+    numberList.innerText = separated;
+    calc()
+  }
+}
+
+add.addEventListener('click', () => {
+  if (input.value == '') {
+    const alertBox = document.getElementById('alert')
+    alertBox.innerHTML = '<div class="alert alert-danger mt-3">Tolong masukkan angka!</div>'
+    setTimeout(() => {
+      alertBox.innerHTML = ''
+    }, 2000)
+  } else {
+    initialResult.push(parseInt(input.value))
+    input.value = ''
+    display()
+  }
+})
+
+display()
